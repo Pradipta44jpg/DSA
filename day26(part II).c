9a9d5f01148cf15e1dc457e73aa5cf9c874c703a
pradiptatech <pradiptasaha7710@gmail.com>
2026-02-11 21:30:57 +0530

    problem 257

--- day26(part II).c
+++ day26(part II).c
@@ -9,7 +9,7 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-void dfs(struct TreeNode *root, int *path, int pathlen,char **result, int *returnSize)
+void dfs(struct TreeNode *root, int *path, int pathlen,char **result1, int *returnSize)
 {
     if(root == NULL)
     return ;
@@ -22,20 +22,20 @@ void dfs(struct TreeNode *root, int *path, int pathlen,char **result, int *retur
         if(i<pathlen-1)
         pos+=sprintf(str+pos,"->");
     }
-    result[*returnSize]=str;
+    result1[*returnSize]=str;
     (*returnSize)++;
     return;
     }
-    dfs(root->left,path,pathlen,result,returnSize);
-    dfs(root->right,path,pathlen,result,returnSize);
+    dfs(root->left,path,pathlen,result1,returnSize);
+    dfs(root->right,path,pathlen,result1,returnSize);
 }
 char** binaryTreePaths(struct TreeNode* root, int* returnSize) {
-   char** result = (char**)malloc(100 * sizeof(char*));
+   char** result1 = (char**)malloc(100 * sizeof(char*));
     int* path = (int*)malloc(100 * sizeof(int));
 
     *returnSize = 0;
 
-    dfs(root, path, 0, result, returnSize);
+    dfs(root, path, 0, result1, returnSize);
 
-    return result;
+    return result1;
 }
