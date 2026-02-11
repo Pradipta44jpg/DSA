abb3de401a3c6014ed98371fe021541f90ebb0c0
pradiptatech <pradiptasaha7710@gmail.com>
2026-02-11 21:28:28 +0530

    problem 257

---
+++ day26(part II).c
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+void dfs(struct TreeNode *root, int *path, int pathlen,char **result, int *returnSize)
+{
+    if(root == NULL)
+    return ;
+    path[pathlen++]=root->val;
+    if(root->left==NULL && root->right== NULL){
+        char *str=(char *)malloc(100* sizeof(char));
+        int pos=0;
+        for(int i=0;i<pathlen;i++){
+            pos+=sprintf(str+pos,"%d",path[i]);
+        if(i<pathlen-1)
+        pos+=sprintf(str+pos,"->");
+    }
+    result[*returnSize]=str;
+    (*returnSize)++;
+    return;
+    }
+    dfs(root->left,path,pathlen,result,returnSize);
+    dfs(root->right,path,pathlen,result,returnSize);
+}
+char** binaryTreePaths(struct TreeNode* root, int* returnSize) {
+   char** result = (char**)malloc(100 * sizeof(char*));
+    int* path = (int*)malloc(100 * sizeof(int));
+
+    *returnSize = 0;
+
+    dfs(root, path, 0, result, returnSize);
+
+    return result;
+}
